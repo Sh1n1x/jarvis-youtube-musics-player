56e81e0c8863de69203561b7399ad33acfaf0ed6
Shinix <cecc.anthony@gmail.com>
2019-03-14 15:55:06 +0100

    Update install.sh
    Change x2 sudo

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 # This file will be automatically sourced at the installation of your plugin
 # Use only if you need to perform changes on the user system such as installing software
 sudo apt-get update && apt-get install vlc nohup
-sudo sudo apt-get remove -y youtube-dl
+sudo apt-get remove -y youtube-dl
 sudo wget https://yt-dl.org/latest/youtube-dl -O /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
